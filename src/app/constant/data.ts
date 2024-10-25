f6eeb6576261bc758fccd7648ac8f1c041dc829f
muqadusbukhari <muqadusbukhari685@gmail.com>
2024-10-25 13:32:19 +0500

    first commit

---
+++ src/app/constant/data.ts
@@ -0,0 +1,32 @@
+export let country_data = {
+    bangladesh: {
+      name: "Bangladesh",
+      population: "170,000,000",
+      capital: "Dhaka",
+      
+    },
+    india: {
+      name: "India",
+      population: "1,38,00,00,000",
+      capital: "Dehli",
+      
+    },
+     japan: {
+        name: "Japan",
+        population: "125,000,000",
+        capital: "Tokyo",
+        
+      },
+      pakistan: {
+        name: "Pakistan",
+        population: "1,38,00,00,000",
+        capital: "Islamabad",
+       
+      },
+      china: {
+        name: "China",
+        population: "1,400,000,000",
+        capital: "Beijing",
+       
+    }
+  };
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className=" text-red-500 text-3xl font-bold ">Hello Welcome to my | Next.js Application | Dynamic Routing Using Components</h1>
+      <h2 className="text-green-500 text-2xl  mt-2 ml-5 m ">
+        My name is Muqadus Bukhari
+      </h2>
+    </div>
+  );
+}
